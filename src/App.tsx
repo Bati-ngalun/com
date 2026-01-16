ebe194669bd3aec4b82b4fdb2937a910d3ea0f9a
Bati-ngalun <zagjammeh20@gmail.com>
2026-01-16 13:02:01 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-      <BrowserRouter basename="/com">
+      <BrowserRouter >
         <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
